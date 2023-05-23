caa09e6c1077ca408fbbf59ac471d25d9e107a3d
jose luis <joseluis.aguilar72@gmail.com>
2023-05-23 10:47:40 -0300

    agrego en views from django.shortcuts import render

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.inicio),
+    path("mitemplate/", views.probando_template),
+
 ]
--- config/views.py
+++ config/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
-from  django.template import Context, Template
+from django.shortcuts import render 
+
+#from  django.template import Context, Template
 
 
 def inicio(request): # el index 
     return HttpResponse("hola ")
+
+
+def probando_template(request):
+   # Es necesario importar las clases Context y Template: from django.template import Context, Template
+    mi_html = open("./templates/template1.html", encoding="utf-8")
+    mi_template = Template(mi_html.read())
+    mi_html.close()
+    mi_contexto = Context()
+    mi_documento = mi_template.render(mi_contexto) 
+    return HttpResponse(mi_documento)
