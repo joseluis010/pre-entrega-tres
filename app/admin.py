d5555b0dadd47325abd2673e575564f63f788d7b
jose luis <joseluis.aguilar72@gmail.com>
2023-05-23 11:24:16 -0300

    cree a los 3 modelos en models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
+
+admin.site.register(models.uno)
+admin.site.register(models.dos)
+admin.site.register(models.tres)
+
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class uno(models.Model):
+    pass
+
+class dos(models.Model):
+    pass
+
+class tres(models.Model):
+    pass
